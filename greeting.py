02b2c3fc01380c4986f0aeec0831a528fd604bfd
robert21-meet <robert21@meet.mit.edu>
2019-07-23 12:36:23 +0300

    Stopping here

---
+++ greeting.py
@@ -0,0 +1,11 @@
+name=input("what is your name:")
+x=name.capitalize()
+print("Hello there, "+x)
+
+name=input("what is your name:")
+length=len(name)
+print("your name is "+str(length)+" letters long")
+print("first letter is  "+name[0])
+print("last letter is "+name[-1])
+print("missing letters "+name[1:-1])
+
